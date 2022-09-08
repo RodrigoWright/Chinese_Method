07764bcea8d1342efcbd8de309da5edaede63da3
RodrigoWright <wright.rodrigo380@gmail.com>
2022-09-08 02:46:48 -0300

    a and n validation

--- src/error/MDCnot1NException.java
+++ src/error/MDCnot1NException.java
@@ -3,8 +3,8 @@ package src.error;
 import javax.swing.JOptionPane;
 
 public class MDCnot1NException extends RuntimeException {
-    public MDCnot1NException() {
+    public MDCnot1NException(int value, int value2) {
         super("O MDC entre os n's informados não é 1");
-        JOptionPane.showMessageDialog(null, "O MDC entre os n informados não é 1");
+        JOptionPane.showMessageDialog(null, "O MDC entre os n informados não é 1: " + value + ", " + value2);
     }
 }
