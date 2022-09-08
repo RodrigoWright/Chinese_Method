81a8236368ac441bc1e2dc08d64afb4202dafc81
RodrigoWright <wright.rodrigo380@gmail.com>
2022-09-08 03:43:19 -0300

    find X

--- src/model/equacaoResto.java
+++ src/model/equacaoResto.java
@@ -4,6 +4,7 @@ public class equacaoResto {
     private int a = 0;
     private int b = 0;
     private int n = 0;
+    private int x = 0;
 
     public int getA(){
         return a;
@@ -13,6 +14,27 @@ public class equacaoResto {
     }
     public int getn(){
         return n;
+    }
+    public int getX(){
+        return x;
+    }
+    public void setX(){
+        boolean valid = false;
+        if (a == b){
+            x = 1;
+            return;
+        };
+        if (a == 1){
+            x = b;
+            return;
+        };
+        for (int i = 1; valid == false ; i++){
+            if (b == ((a*i) % n)){                
+                x = i;
+                valid = true;
+                return;
+            }
+        };
     }
     public void setValues(int x, int y, int z){
         a = x;
