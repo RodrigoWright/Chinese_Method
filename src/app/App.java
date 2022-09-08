52a842faf2b38fd765f5315320850ee815eca3e8
RodrigoWright <wright.rodrigo380@gmail.com>
2022-09-08 00:58:19 -0300

    Get all equation initial values

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,33 @@
 package src.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 import src.model.equacaoResto;
 
 public class App {
-    public static void main (String[] args){
-        System.out.println("Hello World!");
-        equacaoResto[] equacao = new equacaoResto[10];
+    public static void main (String[] args){       
+        List<equacaoResto> equacao = new ArrayList<equacaoResto>();
+        int K = getIntInput("Quantas equações deseja inserir?");
+
+        for (int i = 0; i < K; i++){
+            equacaoResto tmp = new equacaoResto();
+            tmp.setValues(
+                getIntInput("Insira o valor de 'a' da equação " + (i+1)), 
+                getIntInput("Insira o valor de 'b' da equação " + (i+1)), 
+                getIntInput("Insira o valor de 'n' da equação " + (i+1))
+                );
+            equacao.add(tmp);   
+        }
+
+    }
+    private static int getIntInput (String message){
+        int x;
+        String input = JOptionPane.showInputDialog(message);
+        x = Integer.parseInt(input);
+
+        return x;
     }
 }
--- src/model/equacaoResto.java
+++ src/model/equacaoResto.java
@@ -1,7 +1,22 @@
 package src.model;
 
 public class equacaoResto {
-    public void hello(){
-        System.out.println("Hello World!");
+    private int a = 0;
+    private int b = 0;
+    private int n = 0;
+
+    public int getA(){
+        return a;
+    }
+    public int getB(){
+        return b;
+    }
+    public int getN(){
+        return n;
+    }
+    public void setValues(int x, int y, int z){
+        a = x;
+        b = y;
+        n = z;
     }
 }
